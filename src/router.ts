761cf1fc47f12744ced53bff69d6ad9d3cecbb4a
paultirk <paultirk@paultirk.com>
2023-04-20 19:20:23 +0000

    add transaction sidebar

--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router';
 import AccountView from './views/AccountView.vue';
 import DashboardView from './views/DashboardView.vue';
 import Sidebar from './components/Sidebar.vue';
+import TransactionSidebar from './components/TransactionSidebar.vue';
 
 export default new VueRouter({
   linkActiveClass: 'active',
@@ -24,7 +25,26 @@ export default new VueRouter({
             accountId: Number(route.params.accountId)
           };
         }
-      }
+      },
+      children: [
+        {
+          path: 'transaction/:transactionId/details',
+          name: 'transaction-details',
+          meta: {
+            showSidebar: true
+          },
+          components: {
+            sidebar: TransactionSidebar
+          },
+          props: {
+            sidebar: (route) => {
+              return {
+                transactionId: Number(route.params.transactionId)
+              };
+            }
+          }
+        }
+      ]
     }
   ]
 });
